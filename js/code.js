d8377de520d160551bfd6e35d30333718d19cce2
SkywalkerSW5 <50605573+SkywalkerSW5@users.noreply.github.com>
2020-06-10 12:21:16 -0500

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,10 @@
+var code
+code = ["t00no5dv11", "jsng7en4rh"]
+var userCode = windows.prompt("Enter code: ")
+alert ("Code is " + userCode)
+}
+if (code = userCode) {
+    console.log("true")
+} else {
+    console.log("reeeeeeee")
+}
